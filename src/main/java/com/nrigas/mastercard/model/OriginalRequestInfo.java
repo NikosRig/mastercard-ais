60d02cc1e6e517a4d5b22e6003a52acf4cd1778a
Nikos Rigas <37880114+NikosRig@users.noreply.github.com>
2022-11-25 16:33:49 +0100

    consents test added

--- src/main/java/com/nrigas/mastercard/model/OriginalRequestInfo.java
+++ src/main/java/com/nrigas/mastercard/model/OriginalRequestInfo.java
@@ -2,9 +2,5 @@ package com.nrigas.mastercard.model;
 
 public class OriginalRequestInfo {
 
-	private final String xRequestId;
-
-	public OriginalRequestInfo(String xRequestId) {
-		this.xRequestId = xRequestId;
-	}
+	public String xRequestId;
 }
