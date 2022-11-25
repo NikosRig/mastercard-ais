c56b8047abe503a6593485715ad4ad2068e96b69
Nikos Rigas <37880114+NikosRig@users.noreply.github.com>
2022-11-25 16:32:19 +0100

    consents test added

--- src/main/java/com/nrigas/mastercard/model/AuthorizedConsent.java
+++ src/main/java/com/nrigas/mastercard/model/AuthorizedConsent.java
@@ -2,20 +2,8 @@ package com.nrigas.mastercard.model;
 
 public class AuthorizedConsent {
 
-	public final String consentId;
-	public final String consentRequestId;
-	public final OriginalRequestInfo originalRequestInfo;
-	public final String signatureStatus;
-
-	public AuthorizedConsent(
-			String consentId,
-			String consentRequestId,
-			OriginalRequestInfo originalRequestInfo,
-			String signatureStatus
-	) {
-		this.consentId = consentId;
-		this.signatureStatus = signatureStatus;
-		this.consentRequestId = consentRequestId;
-		this.originalRequestInfo = originalRequestInfo;
-	}
+	public String consentId;
+	public String consentRequestId;
+	public OriginalRequestInfo originalRequestInfo;
+	public String signatureStatus;
 }
